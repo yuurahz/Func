588b6fe208def8b229a1ecafda9e771c69fdcbda
Adi <159578677+YuuraHz@users.noreply.github.com>
2024-09-30 22:21:00 +0700

    Update functions.js

--- src/functions.js
+++ src/functions.js
@@ -22,6 +22,16 @@ const {
 
 module.exports = class Function {
 
+clockString = (ms) => {
+   let d = isNaN(ms) ? "--" : Math.floor(ms / 86400000);
+   let h = isNaN(ms) ? "--" : Math.floor(ms / 3600000) % 24;
+   let m = isNaN(ms) ? "--" : Math.floor(ms / 60000) % 60;
+   let s = isNaN(ms) ? "--" : Math.floor(ms / 1000) % 60;
+   return [d, " Day ", h, " hours ", m, " Minutes ", s, " Second "]
+    .map((v) => v.toString().padStart(2, "0"))
+    .join("")
+  }
+
 generateProfilePicture = async (buffer) => {
     const jimp = await Jimp.read(buffer)
     const min = jimp.getWidth()
