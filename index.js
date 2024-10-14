bce89db040c8a89c89e510ad13779037b9fc1a51
Adi <159578677+yuurahz@users.noreply.github.com>
2024-10-14 20:43:48 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ require('dotenv').config()
 const Converter = new (require('./src/converter'))
 const Function = new (require('./src/functions'))
 const Scraper = new (require('./src/scraper'))
+const Levelling = require('./src/levelling')
+const CloudDBAdapter = require('./src/cloudDBAdapter')
 module.exports = class Component {
+   CloudDBAdapter = CloudDBAdapter
    Converter = Converter
    Function = Function
+   levelling = Levelling
    Scraper = Scraper
 }
